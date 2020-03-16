204d38557f75b8776c559e0724e7c014da3544ee
Conor O'Riordan <oriordac1@hawkmail.newpaltz.edu>
2020-03-16 11:07:12 -0400

    Set paths for dist folder in client

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const gameController = require('./controllers/game');
+const path = require('path');
+
 const app = express();
 const port = 3000;
     
 app
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
+    .use(express.static(__dirname + '/../client/dist'))
     .get('/', (req, res) => res.send('This class is awesome'))
     .use('/game', gameController)
+
+    .use((req, res) => {
+        const homePath = path.join(__dirname , '/../client/dist/index.html');
+        res.sendFile(homePath);
+    })
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
