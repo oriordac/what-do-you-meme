935de1478ebc809703d1143baa30e3ff59f035fb
Conor O'Riordan <oriordac1@hawkmail.newpaltz.edu>
2020-04-02 14:38:02 -0400

    Basic login on server-side

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const gameController = require('./controllers/game');
+const userController = require('./controllers/users');
 const path = require('path');
 
 const app = express();
@@ -11,6 +12,7 @@ app
     .use(express.static(__dirname + '/../client/dist'))
     .get('/', (req, res) => res.send('This class is awesome'))
     .use('/game', gameController)
+    .use('/users', userController)
 
     .use((req, res) => {
         const homePath = path.join(__dirname , '/../client/dist/index.html');
--- server/models/Users.js
+++ server/models/Users.js
@@ -1,16 +1,15 @@
 const Users = [
-    { Name: 'Moshe', Password: '2020', Email: 'plotkinm@newpaltz.edu'},
-    { Name: 'Bracha', Password: '5780', Email: 'chabad@newpaltz.edu'}
+    { Name: 'Moshe', Password: '2020', Email: 'plotkinm@newpaltz.edu', userid: 0},
+    { Name: 'Bracha', Password: '5780', Email: 'chabad@newpaltz.edu', userid: 1}
 ];
 
 module.exports = {
-    CurrentUser:null,
 
     Login(email, password) {
         const user = Users.find(x => x.Email == email);
         if(!user) throw Error('User not found');
         if(user.Password != password) throw Error('Wrong Password');
         
-        return this.CurrentUser = user;
+        return user;
     }
 }
